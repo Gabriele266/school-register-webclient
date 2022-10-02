64cc70570b0a0304d6527952ccbdc7febddaf6f6
simo-sw <simonecapelli13@gmail.com>
2022-10-02 17:34:34 +0200

    avanzamento task aggiunta dropdown menu con tabella react-data-table-component, SRW-24

--- src/components/grades/studentsGrade/StudentGradesDialog.tsx
+++ src/components/grades/studentsGrade/StudentGradesDialog.tsx
@@ -1,9 +1,11 @@
-import {Classes, Dialog, Spinner} from "@blueprintjs/core";
+import {Classes, Dialog, MenuItem, Spinner} from "@blueprintjs/core";
 import {useCallback, useEffect, useState} from "react";
 import {instance} from "../../HomePage";
 import {Grade, Student} from "../../../domain/Entities";
-import {Cell, Column, Table2} from "@blueprintjs/table";
 import {msToTime} from "../../utilities/functions/TsFuntions";
+import {ItemRenderer, Select2} from "@blueprintjs/select";
+import DataTable from 'react-data-table-component';
+
 
 interface Props {
     student: Student;
@@ -12,27 +14,132 @@ interface Props {
 }
 
 export const StudentGradesDialog = (props: Props) => {
-    const [grades, setGrades] = useState<Grade[]>([]);
     const [isLoading, setIsLoading] = useState(false);
 
-    useEffect( () => {
+    const [grades, setGrades] = useState<Grade[]>([]);
+    const [subjects, setSubjects] = useState<string[]>([]);
+    const [menuName, setMenuName] = useState("Materia");
+    const [data, setData] = useState<Grade[]>([]);
+
+    useEffect(() => {
         (async () => {
             setIsLoading(true);
 
-            const response = await instance.get(`/students/${ props.student.id }/grades`);
+            const response = await instance.get(`/students/${props.student.id}/grades`);
             setGrades(response.data);
+            setData(response.data);
+            setSubjects(getSubjects(response.data)); // string[] da usare nel dropdown
 
             setIsLoading(false);
         })();
     }, [setIsLoading, setGrades]);
 
+    // Funzione stupenda che restituisce un array di materie(string[]) prese una sola volta per voto
+    function getSubjects(grades: Grade[]): string[] {
+        let arr: string[] = [];
+        let check: boolean = true;
+        grades.forEach(function(element: Grade) {
+            arr.forEach(function (subject: string) {
+                if(subject == element.subject) check = false;
+            });
+            if(check) arr.push(element.subject);
+            check = true;
+        });
+        console.log(arr);
+        return arr;
+    }
 
-    const subjectColumn = (index: number) => {
-        console.log("ciao");
+    // Creazione colonne per tabella voti
+    const columns = [
+        {
+            name: 'Materia',
+            selector: (row: any) => row.subject,
+        },
+        {
+            name: 'Voto',
+            selector: (row: any) => row.value,
+            sortable: true,
+            //sortFunction: ascendingSort
+        },
+        {
+            name: 'Data',
+            selector: (row: any) => msToTime(row.dateTime)
+        },
+        {
+            name: 'Descrizione',
+            selector: (row: any) => row.description
+        }
+    ];
+
+    // Creazione dropdown menu per la selezione della materia
+    const SubjectsSelect = Select2.ofType<string>();
+
+    const renderSubjects: ItemRenderer<string> = (subject, {handleClick}) => {
+        return (
+            <MenuItem
+                text={subject}
+                onClick={handleClick}
+            />
+        );
+    };
+
+    const handleValueChange = (subject: string) => {
+        let array = [];
+        for(let i = 0; i < grades.length; i++) {
+            if(subject == grades[i].subject) array.push(grades[i]);
+        }
+        setMenuName(subject);
+        setData(array);
+    };
+
+    return (
+        <Dialog isOpen={props.isVisible} title="Visualizzazione voti"
+                onClose={props.onClose} className="min-h-min min-w-min">
+            <div className={Classes.DIALOG_BODY}>
+                <nav className="button-group pb-3 flex justify-between">
+                    <div>
+                        <SubjectsSelect items={ subjects }
+                                        itemRenderer={renderSubjects}
+                                        noResults={<MenuItem disabled={true} text="No results."  roleStructure="listoption" />}
+                                        onItemSelect={handleValueChange}>
+                            <button
+                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                { menuName }
+                            </button>
+                        </SubjectsSelect>
+                    </div>
+                    <div>
+                        <button
+                                className="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700">
+                            Crescente
+                        </button>
+                        <button
+                                className="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700">
+                            Decrescente
+                        </button>
+                    </div>
+                </nav>
+
+                <div className="flex-1">
+                    {
+                        isLoading ? <Spinner/> :
+                            <DataTable
+                                columns={columns}
+                                data={data}
+                            />
+                    }
+                </div>
+            </div>
+        </Dialog>
+    );
+}
+/*  OLD TABLE2 NOT WORKING :(
+
+COLUMNS
+const subjectColumn = (index: number) => {
         return <Cell>{grades[index].subject}</Cell>
     }
     const valueColumn = (index: number) => {
-        console.log("ok");
         return <Cell>{grades[index].value}</Cell>
     }
     const dateColumn = (index: number) => {
@@ -42,8 +149,23 @@ export const StudentGradesDialog = (props: Props) => {
         return <Cell>{grades[index].description}</Cell>
     }
 
+SORTING FUNCTIONS
 
-    const ascendingSorted = useCallback(() => {
+    const ascendingSort = (rowA: any, rowB: any) => {
+        const a = rowA.value;
+        const b = rowB.value;
+
+        if (a > b) {
+            return 1;
+        }
+
+        if (b > a) {
+            return -1;
+        }
+
+        return 0;
+    };
+const ascendingSorted = useCallback(() => {
         const arr = grades.slice();
         arr.sort((a, b) => (a.value < b.value ? -1 : 1));
         setGrades(arr);
@@ -51,37 +173,34 @@ export const StudentGradesDialog = (props: Props) => {
 
     }, [grades, setGrades]);
 
+    const descendingSorted = useCallback(() => {
+        const arr = grades.slice();
+        arr.sort((a, b) => (a.value > b.value ? -1 : 1));
+        setGrades(arr);
+        console.log(grades);
+    }, [grades, setGrades]);
 
-    return(
-        <Dialog isOpen={ props.isVisible } title="Visualizzazione voti"
-                onClose={ props.onClose } className="min-h-min min-w-min">
-            <div className={ Classes.DIALOG_BODY }>
-
-                    <nav className="button-group pb-3 flex justify-start">
-                        <button  className="justify-self-start py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700">
-                            Materia</button>
-                        <button onClick={ ascendingSorted } className="py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700">
-                            Crescente</button>
-                        <button className="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700">
-                            Decrescente</button>
-                    </nav>
-
-                <div className="flex-1">
-                    {   //non si aggiorna la table
-                        isLoading ? <Spinner/> :
-                            <Table2 numRows={grades.length}>
-                                <Column name="Materia" cellRenderer={subjectColumn} />
+<Table2 numRows={grades.length}>
+                                <Column name="Materia" cellRenderer={subjectColumn}/>
                                 <Column name="Voto" cellRenderer={valueColumn}/>
                                 <Column name="Data" cellRenderer={dateColumn}/>
                                 <Column name="Descrizione" cellRenderer={descriptionColumn}/>
                             </Table2>
-                    }
-                </div>
-            </div>
-        </Dialog>
-    )
+
+DROPDOWN MENU
+    <SubjectSelect
+    items={Films.items}
+    itemPredicate={Films.itemPredicate}
+    itemRenderer={Films.itemRenderer}
+    noResults={<MenuItem disabled={true} text="No results."  roleStructure="listoption" />}
+    onItemSelect={...}
+        >
+        {// children become the popover target; render value here }
+    <Button text={Films.items[0].title} rightIcon="double-caret-vertical" />
+</SubjectSelect>,
     
 }
+*/
 
 /*
 const handleGrades = useCallback(async () => {
